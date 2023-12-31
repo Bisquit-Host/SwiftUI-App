17de08b1e545f1a7561c4c2f3af33d2c177ba2db
TopScrech <sergei_saliukov@icloud.com>
2024-01-01 00:15:38 +0100

    Optimised biometry button

--- iOS/Settings/Biometry/Biometry Button.swift
+++ iOS/Settings/Biometry/Biometry Button.swift
@@ -5,21 +5,20 @@ struct BiometryButton: View {
     @EnvironmentObject private var settings: SettingsStorage
     
     var body: some View {
-        HStack {
-            Toggle(isOn: $settings.useBiometry) {
-                HStack(alignment: .bottom) {
-                    Text(vm.bioType == "Unknown" ? "Biometry unavailable" : vm.bioType)
-                    
+        Toggle(isOn: $settings.useBiometry) {
+            HStack(alignment: .bottom) {
+                Text(vm.bioType == "Unknown" ? "Biometry unavailable" : vm.bioType)
+                
+                Button {
+                    vm.sheetBio = true
+                } label: {
                     Text("Learn more...")
                         .footnote()
                         .foregroundStyle(.teal)
-                        .onTapGesture {
-                            vm.sheetBio = true
-                        }
                 }
             }
-            .disabled(vm.bioType == "Unknown")
-            .foregroundColor(vm.bioType == "Unknown" ? .gray : .none)
         }
+        .disabled(vm.bioType == "Unknown")
+        .foregroundColor(vm.bioType == "Unknown" ? .gray : .none)
     }
 }
