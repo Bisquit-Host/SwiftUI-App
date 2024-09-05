569a971b3508b22aaa1525e39c2a69766045c8cd
TopScrech <sergei_saliukov@icloud.com>
2024-09-05 12:17:10 +0200

    It works

--- Widgets/New Widget/Asset Fetcher.swift
+++ Widgets/New Widget/Asset Fetcher.swift
@@ -1,4 +1,17 @@
-import Foundation
+import PteroNet
+
+struct WidgetServerListResponse: Decodable {
+    let data: [WidgetServer]
+}
+
+struct WidgetServer: Decodable {
+    let attributes: WidgetServerAttributes
+}
+
+struct WidgetServerAttributes: Decodable {
+    let identifier: String
+    let name: String
+}
 
 struct Asset: Codable {
     let id: String
@@ -23,17 +36,48 @@ struct AssetFetcher {
     }
     
     static func fetchTopTenAssets() async throws -> [Asset] {
-        let url = URL(string: "https://api.coincap.io/v2/assets?limit=10")!
-        
-        // Fetch JSON
-        let (data, _) = try await URLSession.shared.data(from: url)
-        
-        // Parse JSON
-        let response = try JSONDecoder().decode(Response<[Asset]>.self, from: data)
-        
-        let assets = response.data
-        
-        return assets
+        do {
+            let url = URL(string: "https://mgr.bisquit.host/api/client")!
+            
+            // Create a mutable URLRequest
+            var request = URLRequest(url: url)
+            
+            // Fetch the API key from the Keychain
+            if let apiKey = Keychain.load(key: "selectedApiKey") {
+                request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+            } else {
+                print("Error fetching value from Keychain")
+                // Return the test asset if the API key is missing
+                return [
+                    .init(id: "Bitcoin", name: "BTC", symbol: "$10000")
+                ]
+            }
+            
+            let (data, _) = try await URLSession.shared.data(for: request)
+            
+            let response = try JSONDecoder().decode(WidgetServerListResponse.self, from: data)
+            
+            var assets: [Asset] = []
+            
+            for server in response.data.map(\.attributes) {
+                let asset = Asset(
+                    id: "",
+                    name: server.name,
+                    symbol: server.identifier
+                )
+                
+                assets.append(asset)
+            }
+            
+            return assets
+            
+        } catch {
+            let testAssets: [Asset] = [
+                .init(id: "", name: error.localizedDescription, symbol: "Error")
+            ]
+            
+            return testAssets
+        }
     }
     
     static func fetchAssetDetails(_ id: String) async throws -> AssetDetails {
