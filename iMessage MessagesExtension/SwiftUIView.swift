df0e64da9c8578d0d23bda9a1d9709d279e70f39
TopScrech <sergei_saliukov@icloud.com>
2024-09-06 22:00:40 +0200

    -

---
+++ iMessage MessagesExtension/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  iMessage MessagesExtension
+//
+//  Created by Sergei Saliukov on 6/9/24.
+//  Copyright © 2024 Bisquit.Host. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
