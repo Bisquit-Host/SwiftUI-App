646f04f756f0bd2b296cada2aa67a11bdb3cb5cf
TopScrech <sergei_saliukov@icloud.com>
2024-09-07 11:40:08 +0200

    Removed Contact Provider Extension from iMessage

---
+++ iMessage Extension/Server List/Server List VM.swift
@@ -0,0 +1,90 @@
+import ScrechKit
+import PteroNet
+
+@Observable
+final class ServerListVM {
+    // MARK: - PteroNet
+    var servers: [ServerAttributes] = []
+    var apiKey = Keychain.load(key: "selectedApiKey") ?? ""
+    
+    // MARK: - Sheets / Alerts
+    var alertError = false
+    var sheetGuide = false
+    var sheetKeyStorage = false
+    var sheetDiscover = false
+    
+    // MARK: - Filter/Search
+    var searchField = ""
+    var filterBySuspended = false
+    var displayedNode: Node = .all
+    
+    var keys: [String] = []
+    
+    var selectedServer: ServerAttributes?
+    
+    var filteredServers: [ServerAttributes] {
+        servers.filter { server in
+            let prompt = searchField.lowercased()
+            let matchesSearch = searchField.isEmpty || server.name.lowercased().contains(prompt) || server.description.lowercased().contains(prompt)
+            let matchesNode = displayedNode == .all || server.node == displayedNode.rawValue
+            let matchesSuspended = !filterBySuspended || server.isSuspended
+            
+            return matchesSearch && matchesNode && matchesSuspended
+        }
+    }
+    
+    func fetchServers(_ isAdmin: Bool) {
+        serverListAPI(isAdmin) { result in
+            switch result {
+            case .success(let model):
+                guard let model else {
+                    return
+                }
+                
+                let loadedServers = model.data.map(\.attributes)
+                let totalPages = model.meta.pagination.totalPages
+                
+                if totalPages > 1 {
+                    self.fetchAllPages(isAdmin, totalPages: totalPages, currentServers: loadedServers)
+                } else {
+                    withAnimation {
+                        self.servers = loadedServers
+                    }
+                }
+                
+            case .failure(let error):
+                SystemAlert.error(error)
+            }
+        }
+    }
+    
+    private func fetchAllPages(_ isAdmin: Bool, totalPages: Int, currentServers: [ServerAttributes]) {
+        var loadedServers = currentServers
+        let group = DispatchGroup()
+        
+        for page in 2...totalPages {
+            group.enter()
+            
+            serverListAPI(isAdmin, page: page) { result in
+                switch result {
+                case .success(let model):
+                    if let model = model?.data {
+                        let servers = model.map(\.attributes)
+                        loadedServers.append(contentsOf: servers)
+                    }
+                    
+                case .failure(let error):
+                    SystemAlert.error(error)
+                }
+                
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: .main) {
+            withAnimation {
+                self.servers = loadedServers
+            }
+        }
+    }
+}
