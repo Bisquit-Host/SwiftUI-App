435ea5944910df2e751f51e8a7cee45f7d5baed3
TopScrech <sergei_saliukov@icloud.com>
2024-09-08 09:12:45 +0200

    GitHub can be opened in Safari or its dedicated app

--- iOS/Discover/Discover.swift
+++ iOS/Discover/Discover.swift
@@ -20,37 +20,61 @@ struct Discover: View {
                 }
                 
                 Section("Support") {
-                    DiscoverCard(DiscoverItem("App support",
-                                              icon: "questionmark.app.dashed",
-                                              url: "https://topscrech.dev/app/support/",
-                                              color: .purple))
+                    DiscoverCard(
+                        DiscoverItem(
+                            "App support",
+                            icon: "questionmark.app.dashed",
+                            url: "https://topscrech.dev/app/support/",
+                            color: .purple
+                        )
+                    )
                     
-                    DiscoverCard(DiscoverItem("Hosting support",
-                                              icon: "questionmark.bubble",
-                                              url: "https://my.bisquit.host/login",
-                                              color: .purple))
+                    DiscoverCard(
+                        DiscoverItem(
+                            "Hosting support",
+                            icon: "questionmark.bubble",
+                            url: "https://my.bisquit.host/login",
+                            color: .purple
+                        )
+                    )
                 }
                 
                 Section("Apps") {
-                    DiscoverCard(DiscoverItem("More apps by Bisquit.Host",
-                                              icon: "app.gift",
-                                              url: "https://apps.apple.com/au/developer/sergei-saliukov/id1639409936"))
+                    DiscoverCard(
+                        DiscoverItem(
+                            "More apps by Bisquit.Host",
+                            icon: "app.gift",
+                            url: "https://apps.apple.com/au/developer/sergei-saliukov/id1639409936"
+                        )
+                    )
                     
-                    DiscoverCard(DiscoverItem("Web panel",
-                                              icon: "text.and.command.macwindow",
-                                              url: "https://mgr.bisquit.host"))
+                    DiscoverCard(
+                        DiscoverItem(
+                            "Web panel",
+                            icon: "text.and.command.macwindow",
+                            url: "https://mgr.bisquit.host"
+                        )
+                    )
                 }
                 
                 Section("Other") {
-                    DiscoverCard(DiscoverItem("Wiki / FAQ",
-                                              icon: "books.vertical",
-                                              url: "https://wiki.bisquit.host",
-                                              color: .secondary))
+                    DiscoverCard(
+                        DiscoverItem(
+                            "Wiki / FAQ",
+                            icon: "books.vertical",
+                            url: "https://wiki.bisquit.host",
+                            color: .secondary
+                        )
+                    )
                     
-                    DiscoverCard(DiscoverItem("GitHub",
-                                              icon: "app.connected.to.app.below.fill",
-                                              url: "https://github.com/TopScrech",
-                                              color: .secondary))
+                    ListButton(
+                        "GitHub",
+                        icon: "app.connected.to.app.below.fill",
+                        actionIcon: "link",
+                        color: .secondary
+                    ) {
+                        openSafari("https://github.com/TopScrech")
+                    }
                 }
                 
                 NavigationLink {
