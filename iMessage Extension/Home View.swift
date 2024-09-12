c5ef2844d574137977cc3c0f5b5f722c42b1408f
TopScrech <sergei_saliukov@icloud.com>
2024-09-12 23:31:44 +0200

    Revert "Trying to remove the imessage extension"
    This reverts commit 1b3115097976e5c9250ef0eb86ad1949e2570c6f.

---
+++ iMessage Extension/Home View.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+import Messages
+import PteroNet
+
+struct HomeView: View {
+    @StateObject private var settings = SettingsStorage()
+    @State private var serverVm = ServerListVM()
+    @State private var vm: MessagesVM
+    
+    @Binding private var vc: MessagesViewController?
+    
+    init(_ vc: Binding<MessagesViewController?>) {
+        _vc = vc
+        self.vm = .init(vc.wrappedValue)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                ServerList()
+            }
+            .environmentObject(settings)
+            .environment(serverVm)
+            .toolbarBackgroundVisibility(.visible)
+        }
+    }
+}
+
+//#Preview {
+//    HomeView()
+//}
