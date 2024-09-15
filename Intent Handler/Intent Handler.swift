9330bd16a31f4cedfb2c1883e83a8f8f16759c81
TopScrech <sergei_saliukov@icloud.com>
2024-09-15 19:33:32 +0200

    Revert "-"
    This reverts commit 894df060a62b62a2986fb1101e45c8e705da58fc.

--- Intent Handler/Intent Handler.swift
+++ Intent Handler/Intent Handler.swift
@@ -1,36 +1,36 @@
-import Intents
-
-class IntentHandler: INExtension, CryptoPriceConfigurationIntentHandling {
-    func provideSelectedCryptoOptionsCollection(
-        for intent: CryptoPriceConfigurationIntent
-    ) async throws -> INObjectCollection<Crypto> {
-        
-        // Fetch
-        let assets = try await AssetFetcher.fetchTopTenAssets()
-        
-        let servers = assets.map { asset in
-            let server = Crypto(
-                identifier: asset.id,
-                display: "\(asset.name) (\(asset.id))"
-            )
-            
-            server.id = asset.id
-            server.name = asset.name
-            
-            return server
-        }
-        
-        // Create a collection with the array of cryptos
-        let collection = INObjectCollection(items: servers)
-        
-        // Return the collections
-        return collection
-    }
-    
-    override func handler(for intent: INIntent) -> Any {
-        // This is the default implementation. If you want different objects to handle different intents,
-        // you can override this and return the handler you want for that particular intent
-        
-        self
-    }
-}
+//import Intents
+//
+//class IntentHandler: INExtension, CryptoPriceConfigurationIntentHandling {
+//    func provideSelectedCryptoOptionsCollection(
+//        for intent: CryptoPriceConfigurationIntent
+//    ) async throws -> INObjectCollection<Crypto> {
+//        
+//        // Fetch
+//        let assets = try await AssetFetcher.fetchTopTenAssets()
+//        
+//        let servers = assets.map { asset in
+//            let server = Crypto(
+//                identifier: asset.id,
+//                display: "\(asset.name) (\(asset.id))"
+//            )
+//            
+//            server.id = asset.id
+//            server.name = asset.name
+//            
+//            return server
+//        }
+//        
+//        // Create a collection with the array of cryptos
+//        let collection = INObjectCollection(items: servers)
+//        
+//        // Return the collections
+//        return collection
+//    }
+//    
+//    override func handler(for intent: INIntent) -> Any {
+//        // This is the default implementation. If you want different objects to handle different intents,
+//        // you can override this and return the handler you want for that particular intent
+//        
+//        self
+//    }
+//}
