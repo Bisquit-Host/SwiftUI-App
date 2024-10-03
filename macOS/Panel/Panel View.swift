9cf60b91d19cd22ce75be4af9407a697f10fc67f
TopScrech <sergei_saliukov@icloud.com>
2024-10-03 10:15:23 +0200

    -

--- macOS/Panel/Panel View.swift
+++ macOS/Panel/Panel View.swift
@@ -19,7 +19,6 @@ struct PanelView: View {
         //        .info,
         //        .console,
         //        .files,
-        //        .plugins,
         .backups,
         //        .schedules,
         .databases,
