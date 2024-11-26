5ccbe1c8c6695dcdf67bac3943510c5662717871
TopScrech <sergei_saliukov@icloud.com>
2024-11-26 17:15:48 +0100

    -

--- visionOS/Panel/Backups/Backup Card.swift
+++ visionOS/Panel/Backups/Backup Card.swift
@@ -13,51 +13,55 @@ struct BackupCard: View {
     }
     
     var body: some View {
-        HStack {
-            if backup.completedAt != nil {
-                Image(systemName: "doc.zipper")
-                    .title2(.semibold)
-            } else {
-                ZStack {
-                    ProgressView()
-                    
+        Button {
+            
+        } label: {
+            HStack {
+                if backup.completedAt != nil {
                     Image(systemName: "doc.zipper")
                         .title2(.semibold)
-                        .opacity(0)
-                }
-            }
-            
-            VStack(alignment: .leading) {
-                HStack(spacing: 5) {
-                    if backup.isLocked {
-                        Image(systemName: "lock")
-                            .foregroundStyle(.orange)
+                } else {
+                    ZStack {
+                        ProgressView()
+                        
+                        Image(systemName: "doc.zipper")
+                            .title2(.semibold)
+                            .opacity(0)
                     }
-                    
-                    Text(backup.name)
-                        .lineLimit(1)
-#if os(iOS)
-                        .minimumScaleFactor(0.75)
-                        .scaledToFit()
-#endif
                 }
-                .animation(.default, value: backup.isLocked)
-                .headline()
                 
-                let timeDifference = Text(timeSinceISO(backup.createdAt))
-                    .foregroundStyle(.primary)
+                VStack(alignment: .leading) {
+                    HStack(spacing: 5) {
+                        if backup.isLocked {
+                            Image(systemName: "lock")
+                                .foregroundStyle(.orange)
+                        }
+                        
+                        Text(backup.name)
+                            .lineLimit(1)
+#if os(iOS)
+                            .minimumScaleFactor(0.75)
+                            .scaledToFit()
+#endif
+                    }
+                    .animation(.default, value: backup.isLocked)
+                    .headline()
+                    
+                    let timeDifference = Text(timeSinceISO(backup.createdAt))
+                        .foregroundStyle(.primary)
+                    
+                    Text("Created: \(timeDifference)")
+                        .footnote()
+                        .secondary()
+                        .minimumScaleFactor(0.5)
+                        .lineLimit(1)
+                }
                 
-                Text("Created: \(timeDifference)")
-                    .footnote()
+                Spacer()
+                
+                Text(formatBytes(backup.bytes))
                     .secondary()
-                    .minimumScaleFactor(0.5)
-                    .lineLimit(1)
             }
-            
-            Spacer()
-            
-            Text(formatBytes(backup.bytes))
-                .secondary()
         }
         .safariCover($cardVm.showSafari, url: cardVm.url)
         .swipeActions {
