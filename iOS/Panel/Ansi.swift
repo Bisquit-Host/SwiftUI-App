00e9bda47562e1393e5a00ef2da814e8f482e6c5
TopScrech <sergei_saliukov@icloud.com>
2025-01-19 01:22:55 +0100

    -

--- iOS/Panel/Ansi.swift
+++ iOS/Panel/Ansi.swift
@@ -3,23 +3,23 @@ import SwiftUI
 fileprivate let regex = try! NSRegularExpression(pattern: "\\x1b\\[[0-9;]*m")
 fileprivate let regex1 = try! Regex("\\x1b\\[[0-9;]*m")
 
-fileprivate let ansiToSwiftUIColorMap: [Int: Color] = [
-    30: Color(0xFF131a20),  // Black
-    31: Color(0xFFFE5370),  // Red
-    32: Color(0xFFC2E78C),  // Green
-    33: Color(0xFFFECA6B),  // Yellow
-    34: Color(0xFF396FE2),  // Blue
-    35: Color(0xFFBB80B3),  // Magenta
-    36: Color(0xFF88DCFE),  // Cyan
-    37: Color(0xFFD0D0D0),  // White
-    90: Color(0xFF333333),  // Bright Black
-    91: Color(0xFFFF5370),  // Bright Red
-    92: Color(0xFFC3E88D),  // Bright Green
-    93: Color(0xFFFFCB6B),  // Bright Yellow
-    94: Color(0xFF82AAFF),  // Bright Blue
-    95: Color(0xFFC792EA),  // Bright Magenta
-    96: Color(0xFF89DDFF),  // Bright Cyan
-    97: .white,             // Bright White
+fileprivate let ansiToSwiftUIColorMap = [
+    30: Color(0xFF131a20), // Black
+    31: Color(0xFFFE5370), // Red
+    32: Color(0xFFC2E78C), // Green
+    33: Color(0xFFFECA6B), // Yellow
+    34: Color(0xFF396FE2), // Blue
+    35: Color(0xFFBB80B3), // Magenta
+    36: Color(0xFF88DCFE), // Cyan
+    37: Color(0xFFD0D0D0), // White
+    90: Color(0xFF333333), // Bright Black
+    91: Color(0xFFFF5370), // Bright Red
+    92: Color(0xFFC3E88D), // Bright Green
+    93: Color(0xFFFFCB6B), // Bright Yellow
+    94: Color(0xFF82AAFF), // Bright Blue
+    95: Color(0xFFC792EA), // Bright Magenta
+    96: Color(0xFF89DDFF), // Bright Cyan
+    97: .white,            // Bright White
 ]
 
 public func convertAnsiToAttributedString(_ input: String) -> AttributedString {
@@ -54,6 +54,7 @@ public func convertAnsiToAttributedString(_ input: String) -> AttributedString {
         
         if idx < matches.count {
             let match = matches[idx]
+            
             let colorCode = (input as NSString)
                 .substring(with: match.range)
                 .dropFirst(2)
@@ -104,11 +105,18 @@ public func convertAnsiToAttributedString(_ input: String) -> AttributedString {
 }
 
 private func detectAndAddLinks(_ attributedString: inout AttributedString) {
-    let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+    let detector = try! NSDataDetector(
+        types: NSTextCheckingResult.CheckingType.link.rawValue
+    )
     
-    let mutableAttributedString = NSMutableAttributedString(attributedString: NSAttributedString(attributedString))
+    let mutableAttributedString = NSMutableAttributedString(
+        attributedString: .init(attributedString)
+    )
     
-    let fullRange = NSRange(location: 0, length: mutableAttributedString.length)
+    let fullRange = NSRange(
+        location: 0,
+        length: mutableAttributedString.length
+    )
     
 #if os(macOS)
     let urlColor = NSColor.blue
@@ -117,8 +125,10 @@ private func detectAndAddLinks(_ attributedString: inout AttributedString) {
 #endif
     
     detector.enumerateMatches(in: mutableAttributedString.string, options: [], range: fullRange) { match, _, _ in
-        guard let match,
-              let url = URL(string: (mutableAttributedString.string as NSString).substring(with: match.range)) else {
+        guard
+            let match,
+            let url = URL(string: (mutableAttributedString.string as NSString).substring(with: match.range))
+        else {
             return
         }
         
