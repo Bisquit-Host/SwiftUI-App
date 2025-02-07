0a38997c9429d3a20b8dbec4439f8975b4bbba73
TopScrech <sergei_saliukov@icloud.com>
2025-02-07 02:39:52 +0100

    -

--- iOS/Server List/Server List VM.swift
+++ iOS/Server List/Server List VM.swift
@@ -1,10 +1,6 @@
 import ScrechKit
 import PteroNet
 
-#if canImport(CoreSpotlight)
-import CoreSpotlight
-#endif
-
 @Observable
 final class ServerListVM {
     // MARK: - PteroNet
@@ -119,65 +115,7 @@ final class ServerListVM {
             }
         }
     }
-    
-#if os(iOS)
-    private func fetchUniqueUsers() {
-        let ids = servers.map(\.id)
         
-        var allUsers: [UserAttributes] = []
-        let dispatchGroup = DispatchGroup()
-        let queue = DispatchQueue(label: "host.bisquit.uniqueUsersQueue")
-        
-        for id in ids {
-            dispatchGroup.enter()
-            
-            fetchUsers(id) { users in
-                guard let users else {
-                    dispatchGroup.leave()
-                    return
-                }
-                
-                queue.async {
-                    for user in users {
-                        if !allUsers.contains(where: { $0.email == user.email }) {
-                            allUsers.append(user)
-                        }
-                    }
-                    
-                    dispatchGroup.leave()
-                }
-            }
-        }
-        
-        dispatchGroup.notify(queue: .main) {
-            let niggers = allUsers
-            
-            Task {
-                await self.saveContacts(niggers)
-            }
-        }
-    }
-    
-    private func fetchUsers(_ id: String, completion: @escaping ([UserAttributes]?) -> Void) {
-        userListAPI(id) { result in
-            switch result {
-            case .success(let model):
-                guard let model = model?.data else {
-                    completion(nil)
-                    return
-                }
-                
-                let attributes = model.map(\.attributes)
-                completion(attributes)
-                
-            case .failure(let error):
-                SystemAlert.error(error)
-                completion(nil)
-            }
-        }
-    }
-#endif
-    
     private func fetchAllPages(_ isAdmin: Bool, totalPages: Int, currentServers: [ServerAttributes]) {
         var loadedServers = currentServers
         let group = DispatchGroup()
