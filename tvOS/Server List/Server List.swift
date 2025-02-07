aa4709b5d55cc913122ee0fbc0fbe9d60b95d51f
TopScrech <sergei_saliukov@icloud.com>
2025-02-07 11:43:37 +0100

    tvSO: toolbar in server list

--- tvOS/Server List/Server List.swift
+++ tvOS/Server List/Server List.swift
@@ -1,39 +1,14 @@
-import SwiftUI
+import ScrechKit
 
 struct ServerList: View {
     @Environment(ServerListVM.self) private var vm
     @Environment(NavState.self) private var navState
     @EnvironmentObject private var store: ValueStore
     
-    private var adminButtonColor: Color {
-        store.adminServerList ? .green : .secondary
-    }
-    
     @State private var sheetOverview = false
     
     var body: some View {
         List {
-            if store.devMode {
-                Button {
-                    store.adminServerList.toggle()
-                    vm.fetchServers(store.adminServerList)
-                } label: {
-                    Label("Admin", systemImage: "person.badge.shield.checkmark")
-                        .foregroundStyle(adminButtonColor)
-                }
-#if DEBUG
-                Button("Overview") {
-                    sheetOverview = true
-                }
-#endif
-            }
-            
-            NavigationLink {
-                Settings()
-            } label: {
-                Text("\(Image(systemName: "gear")) Settings")
-            }
-            
             ForEach(vm.filteredServers) { server in
                 Button {
                     navState.navigate(.toPanel(server.id))
@@ -45,6 +20,26 @@ struct ServerList: View {
         .background(BisquitFall())
         .task {
             vm.fetchServers(store.adminServerList)
+        }
+        .toolbar {
+            NavigationLink("Settings") {
+                Settings()
+            }
+            
+            if store.devMode {
+                Button {
+                    store.adminServerList.toggle()
+                    vm.fetchServers(store.adminServerList)
+                } label: {
+                    Text("Admin")
+                        .foregroundStyle(store.adminServerList ? .primary : .secondary)
+                }
+#if DEBUG
+                Button("Overview") {
+                    sheetOverview = true
+                }
+#endif
+            }
         }
 #if DEBUG
         .sheet($sheetOverview) {
