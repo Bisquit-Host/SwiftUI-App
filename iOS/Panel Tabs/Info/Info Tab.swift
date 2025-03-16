8aa799a82f8746a6c27186b7e63492d8d0893e44
TopScrech <sergei_saliukov@icloud.com>
2025-03-16 02:40:40 +0100

    InfoTab button fix

--- iOS/Panel Tabs/Info/Info Tab.swift
+++ iOS/Panel Tabs/Info/Info Tab.swift
@@ -21,65 +21,52 @@ struct InfoTab: View {
                     .frame(width: width, height: width)
                     .clipped()
                 
-                Image(.darkBackgroundInfo)
-                //                Image(.defaultIcon)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: width * 1.1)
-                    .frame(maxHeight: 600)
-                    .clipped()
-                    .blur(radius: 55, opaque: true)
-                    .offset(y: -30)
-                    .blur(radius: 10)
-                    .overlay(alignment: .top) {
-                        VStack(spacing: 10) {
-                            VStack(spacing: 5) {
-                                Text(server.name)
-                                    .largeTitle(.bold)
-                                    .lineLimit(1)
-                                
-                                Text(server.description)
-                                    .secondary()
-                                    .lineLimit(1)
-                                
-                                Text("\(server.id) • \(server.node)")
-                                    .caption2()
-                                    .foregroundStyle(.tertiary)
-                            }
-                            .rounded()
-                            .offset(y: -16)
+                ZStack(alignment: .top) {
+                    Image(.darkBackgroundInfo)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: width * 1.1)
+                        .frame(maxHeight: 800)
+                        .clipped()
+                        .blur(radius: 55, opaque: true)
+                        .offset(y: -30)
+                        .blur(radius: 10)
+                    
+                    VStack(spacing: 10) {
+                        VStack(spacing: 5) {
+                            Text(server.name)
+                                .largeTitle(.bold)
+                                .lineLimit(1)
                             
-                            InfoTabCard(server)
-#warning("Tip")
-                            InfoTabAllocation(server)
+                            Text(server.description)
+                                .secondary()
+                                .lineLimit(1)
                             
-                            InfoTabButtons(server)
-                            
-#if canImport(ActivityKit)
-            InfoTabLAButton(server)
-#endif
+                            Text("\(server.id) • \(server.node)")
+                                .caption2()
+                                .foregroundStyle(.tertiary)
                         }
-                        .padding(.horizontal, 10)
-                        .frame(width: width)
+                        .rounded()
+                        
+                        InfoTabCard(server)
+#warning("Tip")
+                        InfoTabAllocation(server)
+                        
+                        InfoTabButtons(server)
+                        
+#if canImport(ActivityKit)
+                        InfoTabLAButton(server)
+#endif
                     }
-                
-                Image(.darkBackgroundInfo)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: width * 1.1)
-                    .frame(maxHeight: 600)
-                    .clipped()
-                    .blur(radius: 55, opaque: true)
-                    .offset(y: 60)
-                    .blur(radius: 10)
-                    .scaleEffect(x: 1, y: -1)
+                    .padding(.horizontal, 10)
+                    .frame(width: width)
+                }
                 
                 // Text("А что вы делаете в моём холодильнике? Может вы хотите кушать?")
             }
         }
         .ignoresSafeArea()
         .toolbarBackground(.visible, for: .tabBar)
-        .frame(maxWidth: 500)
     }
 }
 
