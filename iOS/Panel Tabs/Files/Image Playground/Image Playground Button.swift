87590daefb5e03de767d84d165411181d96d9e23
TopScrech <sergei_saliukov@icloud.com>
2025-03-16 19:33:57 +0100

    Image Playground button fixed

--- iOS/Panel Tabs/Files/Image Playground/Image Playground Button.swift
+++ iOS/Panel Tabs/Files/Image Playground/Image Playground Button.swift
@@ -17,42 +17,20 @@ struct ImagePlaygroundButton: View {
             sheetImagePlayground = true
         } label: {
             ZStack {
-//                if supportsImagePlayground {
+                if supportsImagePlayground {
                     Image(.appleIntelligence)
                         .resizable()
                         .frame(width: 35, height: 35)
                         .clipShape(.circle)
                         .blur(radius: 3)
-//                }
+                }
                 
                 Image(.appleIntelligence)
                     .resizable()
                     .frame(width: 32, height: 32)
                     .clipShape(.circle)
-//                    .opacity(supportsImagePlayground ? 1 : 0.2)
+                    .opacity(supportsImagePlayground ? 1 : 0.2)
             }
-            //            HStack {
-            //                Text("Image Playground")
-            //                    .semibold()
-            //                    .rounded()
-            //
-            //                Spacer()
-            //
-            //                ZStack {
-            //                    if supportsImagePlayground {
-            //                        Image(.appleIntelligence)
-            //                            .resizable()
-            //                            .frame(width: 28, height: 28)
-            //                            .blur(radius: 3)
-            //                    }
-            //
-            //                    Image(.appleIntelligence)
-            //                        .resizable()
-            //                        .frame(width: 25, height: 25)
-            //                        .frame(width: 25, height: 25)
-            //                        .opacity(supportsImagePlayground ? 1 : 0.3)
-            //                }
-            //            }
         }
         .keyboardShortcut("P")
         .foregroundStyle(.foreground)
