91deef66c4156c57ca22c2d8c01259345cb29907
TopScrech <sergei_saliukov@icloud.com>
2025-03-25 14:05:04 +0100

    -

--- iOS/Intro/Infinite Scroll View.swift
+++ iOS/Intro/Infinite Scroll View.swift
@@ -9,12 +9,12 @@ struct InfiniteScrollView<Content: View>: View {
     
     var body: some View {
         GeometryReader {
-            let size = $0.size
+            let width = $0.size.width
             
             ScrollView(.horizontal) {
                 HStack(spacing: spacing) {
                     Group(subviews: content) { collection in
-                        /// Original Content
+                        /// Posters
                         HStack(spacing: spacing) {
                             ForEach(collection) { view in
                                 view
@@ -28,7 +28,7 @@ struct InfiniteScrollView<Content: View>: View {
                         
                         /// Repeating Content for creating Infinite(Looping) ScrollView
                         let averageWidth = contentSize.width / CGFloat(collection.count)
-                        let repeatingCount = contentSize.width > 0 ? Int((size.width / averageWidth).rounded()) + 1 : 1
+                        let repeatingCount = contentSize.width > 0 ? Int((width / averageWidth).rounded()) + 1 : 1
                         
                         HStack(spacing: spacing) {
                             ForEach(0..<repeatingCount, id: \.self) { index in
