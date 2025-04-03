dfb4ac776dfd7d29f5616b65e6cd2e05645521e4
TopScrech <sergei_saliukov@icloud.com>
2025-04-03 18:51:21 +0200

    -

---
+++ iOS/Panel Tabs/Logs/Log Toolbar Actor.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+import PteroNet
+
+struct LogToolbarActor: View {
+    @Environment(LogVM.self) private var vm
+    
+    private let actor: LogRelationships?
+    
+    init(_ actor: LogRelationships?) {
+        self.actor = actor
+    }
+    
+    var body: some View {
+        if let actor {
+            Button {
+                if vm.selectedActor == actor {
+                    vm.selectedActor = nil
+                } else {
+                    vm.selectedActor = actor
+                }
+            } label: {
+                if let username = actor.actor.attributes?.username {
+                    Text(username)
+                    
+                    if let email = actor.actor.attributes?.email {
+                        Text(email)
+                    }
+                } else {
+                    Text("System")
+                }
+                
+                if vm.selectedActor == actor {
+                    Image(systemName: "checkmark")
+                }
+            }
+        }
+    }
+}
+
+//#Preview {
+//    LogToolbarActor()
+//        .environment(LogVM())
+//}
