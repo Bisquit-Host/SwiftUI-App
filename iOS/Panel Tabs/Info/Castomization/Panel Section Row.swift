be296831190bcb7d0cfcb389adbb9b6bac1cc061
TopScrech <sergei_saliukov@icloud.com>
2025-05-01 10:22:58 +0200

    -

--- iOS/Panel Tabs/Info/Castomization/Panel Section Row.swift
+++ iOS/Panel Tabs/Info/Castomization/Panel Section Row.swift
@@ -20,21 +20,6 @@ struct PanelSectionRow: View {
                 Text(item.name)
             }
             .title3(.semibold)
-            
-            if item.name == "Location" {
-                HStack {
-                    Image(systemName: item.isChecked ? "checkmark.circle.fill" : "circle")
-                        .onTapGesture {
-                            toggle()
-                        }
-                    
-                    Text("Show Ping")
-                }
-                .fontSize(14)
-                .secondary()
-                .padding(.horizontal)
-                .padding(.vertical, 5)
-            }
         }
     }
 }
