8a1ac429165baf32c5fea1c998b6e3f923aa5ab0
TopScrech <sergei_saliukov@icloud.com>
2025-05-01 14:26:15 +0200

    Gamepad controlling moved to a separate lib

--- iOS/Settings/Debug & Dev/Gamepad Debug.swift
+++ iOS/Settings/Debug & Dev/Gamepad Debug.swift
@@ -1,26 +1,28 @@
 import SwiftUI
+import GaypadKit
 
 struct GamepadDebug: View {
     @State private var gamepad = GamepadManager.shared
     
     var body: some View {
         VStack(spacing: 12) {
-            Text(gamepad.isAPressed ? "A Pressed" : "Press A") // PS X // Primary Action
-            Text(gamepad.isBPressed ? "B Pressed" : "Press B") // PS Circle // Back
-            Text(gamepad.isXPressed ? "X Pressed" : "Press X") // PS Square
-            Text(gamepad.isYPressed ? "Y Pressed" : "Press Y") // PS Triangle // Toolbar
-            Text(gamepad.isMenuPressed ? "Menu Pressed" : "Press Menu")
-            Text(gamepad.isOptionsPressed ? "Options Pressed" : "Press Options")
-            Text(gamepad.isLeftThumbstickPressed ? "Left Stick Pressed" : "Press Left Stick")
-            Text(gamepad.isRightThumbstickPressed ? "Right Stick Pressed" : "Press Right Stick")
-            Text(gamepad.isLeftShoulderPressed ? "Left Shoulder Pressed" : "Press Left Shoulder")
-            Text(gamepad.isRightShoulderPressed ? "Right Shoulder Pressed" : "Press Right Shoulder")
-            Text(gamepad.isLeftTriggerPressed ? "Left Trigger Pressed" : "Press Left Trigger")
-            Text(gamepad.isRightTriggerPressed ? "Right Trigger Pressed" : "Press Right Trigger")
-            Text(gamepad.isDpadUpPressed ? "DPad Up Pressed" : "Press DPad Up")
-            Text(gamepad.isDpadDownPressed ? "DPad Down Pressed" : "Press DPad Down")
-            Text(gamepad.isDpadLeftPressed ? "DPad Left Pressed" : "Press DPad Left")
-            Text(gamepad.isDpadRightPressed ? "DPad Right Pressed" : "Press DPad Right")
+            Text(gamepad.aPressed ? "A Pressed" : "Press A") // PS X // Primary Action
+            Text(gamepad.bPressed ? "B Pressed" : "Press B") // PS Circle // Back
+            Text(gamepad.xPressed ? "X Pressed" : "Press X") // PS Square
+            Text(gamepad.yPressed ? "Y Pressed" : "Press Y") // PS Triangle // Toolbar
+            Text(gamepad.menuPressed ? "Menu Pressed" : "Press Menu")
+            Text(gamepad.optionsPressed ? "Options Pressed" : "Press Options")
+            Text(gamepad.leftThumbstickPressed ? "Left Stick Pressed" : "Press Left Stick")
+            Text(gamepad.rightThumbstickPressed ? "Right Stick Pressed" : "Press Right Stick")
+            Text(gamepad.leftShoulderPressed ? "Left Shoulder Pressed" : "Press Left Shoulder")
+            Text(gamepad.rightShoulderPressed ? "Right Shoulder Pressed" : "Press Right Shoulder")
+            Text(gamepad.leftTriggerPressed ? "Left Trigger Pressed" : "Press Left Trigger")
+            Text(gamepad.rightTriggerPressed ? "Right Trigger Pressed" : "Press Right Trigger")
+            
+            Text(gamepad.dpadUpPressed ? "DPad Up Pressed" : "Press DPad Up")
+            Text(gamepad.dpadDownPressed ? "DPad Down Pressed" : "Press DPad Down")
+            Text(gamepad.dpadLeftPressed ? "DPad Left Pressed" : "Press DPad Left")
+            Text(gamepad.dpadRightPressed ? "DPad Right Pressed" : "Press DPad Right")
             
             Text("Battery: \(Int(gamepad.batteryLevel * 100))%")
         }
