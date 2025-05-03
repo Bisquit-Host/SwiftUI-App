e5f5196c88157a5b739231bc285d707b14c6ff28
TopScrech <sergei_saliukov@icloud.com>
2025-05-03 14:55:38 +0200

    -

--- macOS/Path Nav/Models/Data Model.swift
+++ macOS/Path Nav/Models/Data Model.swift
@@ -7,26 +7,12 @@ import PteroNet
 final class DataModel {
     private(set) var servers: [ServerAttributes] = []
     
-    private var recipesById: [ServerAttributes.ID: ServerAttributes] = [:]
+    private var serversById: [ServerAttributes.ID: ServerAttributes] = [:]
     
     /// The shared singleton data model object
     static let shared: DataModel = {
         DataModel()
     }()
-    
-    private static var dataURL: URL {
-        get throws {
-            let bundle = Bundle.main
-            
-            guard
-                let path = bundle.path(forResource: "Recipes", ofType: "json")
-            else {
-                throw CocoaError(.fileReadNoSuchFile)
-            }
-            
-            return URL(fileURLWithPath: path)
-        }
-    }
     
     func fetchServers(_ isAdmin: Bool) {
         serverListAPI(isAdmin) { result in
@@ -48,16 +34,9 @@ final class DataModel {
         }
     }
     
-    //    /// The servers for a given category, sorted by name
-    //    func recipes(in category: Category?) -> [ServerAttributes] {
-    //        servers.filter {
-    //            $0.category == category
-    //        }
-    //    }
-    
     /// Accesses the recipe associated with the given unique identifier
     /// if the identifier is tracked by the data model; otherwise, returns `nil`
     subscript(recipeId: ServerAttributes.ID) -> ServerAttributes? {
-        recipesById[recipeId]
+        serversById[recipeId]
     }
 }
