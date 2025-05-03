eeac656e25d8f94fddc0ce99f4522b1664487e1c
TopScrech <sergei_saliukov@icloud.com>
2025-05-03 13:18:13 +0200

    -

--- iOS/Server List/Server List VM.swift
+++ iOS/Server List/Server List VM.swift
@@ -2,26 +2,6 @@ import ScrechKit
 import PteroNet
 import GameKit
 
-extension UserDefaults {
-    func setServerAttributesArray(_ servers: [ServerAttributes], forKey key: String) {
-        let encoder = JSONEncoder()
-        
-        if let data = try? encoder.encode(servers) {
-            set(data, forKey: key)
-        }
-    }
-    
-    func serverAttributesArray(forKey key: String) -> [ServerAttributes]? {
-        guard let data = data(forKey: key) else {
-            return nil
-        }
-        
-        let decoder = JSONDecoder()
-        
-        return try? decoder.decode([ServerAttributes].self, from: data)
-    }
-}
-
 @Observable
 final class ServerListVM {
     // MARK: - PteroNet
