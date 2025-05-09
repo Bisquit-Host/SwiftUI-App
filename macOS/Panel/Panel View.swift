8e3ea0d2b6cd0e641eac14f4838ead33ed5c28b6
TopScrech <sergei_saliukov@icloud.com>
2025-05-09 12:02:58 +0200

    -

--- macOS/Panel/Panel View.swift
+++ macOS/Panel/Panel View.swift
@@ -12,7 +12,6 @@ struct PanelView: View {
         self.vm = PanelVM(server.id)
     }
     
-    #warning("Move to the ValueStore")
     @AppStorage("selected_tab") private var selectedTab: PanelTab = .info
     
     private let gradient = Gradient(colors: [
@@ -50,7 +49,7 @@ struct PanelView: View {
         VStack(spacing: 0) {
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack {
-                    ForEach(tabs, id: \.self) { tab in
+                    ForEach(tabs) { tab in
                         Button {
                             selectedTab = tab
                         } label: {
