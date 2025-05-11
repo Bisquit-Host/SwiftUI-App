d6bbdc6a462af70745c4540f70ec4dbf46e48e30
TopScrech <sergei_saliukov@icloud.com>
2025-05-11 08:38:13 +0200

    created QuikLook extension

---
+++ QuickLook Extension/PreviewViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PreviewViewController.swift
+//  QuickLook Extension
+//
+//  Created by Sergei Saliukov on 11.05.2025.
+//  Copyright © 2025 Bisquit.Host. All rights reserved.
+//
+
+import Cocoa
+import Quartz
+
+class PreviewViewController: NSViewController, QLPreviewingController {
+
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("PreviewViewController")
+    }
+
+    override func loadView() {
+        super.loadView()
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    func preparePreviewOfSearchableItem(identifier: String, queryString: String?) async throws {
+        // Implement this method and set QLSupportsSearchableItems to YES in the Info.plist of the extension if you support CoreSpotlight.
+
+        // Perform any setup necessary in order to prepare the view.
+        // Quick Look will display a loading spinner until this returns.
+    }
+    */
+
+    func preparePreviewOfFile(at url: URL) async throws {
+        // Add the supported content types to the QLSupportedContentTypes array in the Info.plist of the extension.
+
+        // Perform any setup necessary in order to prepare the view.
+
+        // Quick Look will display a loading spinner until this returns.
+    }
+
+}
