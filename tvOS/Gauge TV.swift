14e2aa14d0523153bd9b676eb8afc3cfc6b3137b
TopScrech <sergei_saliukov@icloud.com>
2025-05-13 12:18:58 +0200

    tvOS: Improved info tab

--- tvOS/Gauge TV.swift
+++ tvOS/Gauge TV.swift
@@ -10,12 +10,15 @@ struct GaugeTV: View {
     
     var body: some View {
         HStack(spacing: 0) {
-            Text(name.capitalized + ": ")
+            Text(name.capitalized)
+            
+            Spacer()
             
             Text(parameter)
                 .bold()
         }
         .title3()
-        .padding(.leading, 30)
+        .padding(.horizontal, 30)
+        .padding(.trailing, 60)
     }
 }
