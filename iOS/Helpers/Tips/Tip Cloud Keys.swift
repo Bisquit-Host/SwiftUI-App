45ba2916c57734c71afba32818c81040f4fc0817
TopScrech <sergei_saliukov@icloud.com>
2025-05-16 10:03:26 +0200

    Loc

--- iOS/Helpers/Tips/Tip Cloud Keys.swift
+++ iOS/Helpers/Tips/Tip Cloud Keys.swift
@@ -6,7 +6,7 @@ struct Tip_CloudKeys: Tip {
     }
     
     var message: Text? {
-        Text("iCloud ensures the secure and efficient storage of your precious data, including API-keys")
+        Text("iCloud ensures the secure storage of your precious data, including API-keys")
     }
     
     var image: Image? {
