941847cc95ceebe120322d4560c7af40069ed506
TopScrech <sergei_saliukov@icloud.com>
2025-06-11 00:24:52 +0200

    improved panel toolbar

--- iOS/Panel/Panel Toolbar.swift
+++ iOS/Panel/Panel Toolbar.swift
@@ -11,58 +11,65 @@ struct PanelToolbarModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
             .toolbar {
-                //                ToolbarItemGroup(placement: .topBarTrailing) {
-                if store.lastTabPanel == .console {
-                    Button {
-                        withAnimation {
-                            vm.enableConsoleSearch.toggle()
+                ToolbarItemGroup {
+                    if store.lastTabPanel == .console {
+                        Button {
+                            withAnimation {
+                                vm.enableConsoleSearch.toggle()
+                            }
+                            
+                            if vm.enableConsoleSearch {
+                                vm.searchRule = consoleVM.command
+                            }
+                        } label: {
+                            let icon = vm.enableConsoleSearch ? "magnifyingglass.circle.fill" : "magnifyingglass"
+                            
+                            Image(systemName: icon)
                         }
                         
-                        if vm.enableConsoleSearch {
-                            vm.searchRule = consoleVM.command
+                        Button {
+                            consoleVM.inspectorPresented = true
+                        } label: {
+                            Image(systemName: "bold.italic.underline")
+                        }
+                    }
+                    
+                    if store.lastTabPanel == .info {
+                        PowerSwitchToolbar()
+                        
+                        if let server = vm.server {
+#if canImport(ActivityKit)
+                            InfoTabLA(server)
+#endif
+                        }
+                    }
+                    
+                    if store.lastTabPanel == .files {
+                        if #available(iOS 18.1, *) {
+                            ImagePlaygroundButton(fileVM.path)
+                        }
+                        
+                        Button {
+                            vm.alertNewFolder = true
+                        } label: {
+                            Image(systemName: "folder.badge.plus")
+                        }
+                    }
+                }
+                
+                ToolbarSpacer()
+                
+                ToolbarItem {
+                    Button {
+                        withAnimation(.easeOut) {
+                            vm.sheetSettings = true
                         }
                     } label: {
-                        Image(systemName: vm.enableConsoleSearch ? "magnifyingglass.circle.fill" : "magnifyingglass")
+                        Image(systemName: "ellipsis")
+                            .offset(x: 4)
                     }
-                    
-                    Button {
-                        consoleVM.inspectorPresented = true
-                    } label: {
-                        Image(systemName: "bold.italic.underline")
-                    }
+                    .keyboardShortcut("S")
                 }
-                
-                if store.lastTabPanel == .info {
-                    PowerSwitchToolbar()
-                    
-                    if let server = vm.server {
-#if canImport(ActivityKit)
-                        InfoTabLA(server)
-#endif
-                    }
-                }
-                
-                if store.lastTabPanel == .files {
-                    if #available(iOS 18.1, *) {
-                        ImagePlaygroundButton(fileVM.path)
-                    }
-                    
-                    Button {
-                        vm.alertNewFolder = true
-                    } label: {
-                        Image(systemName: "folder.badge.plus")
-                    }
-                }
-                
-                Button {
-                    withAnimation(.easeOut) {
-                        vm.sheetSettings = true
-                    }
-                } label: {
-                    Image(systemName: "ellipsis")
-                }
-                .keyboardShortcut("S")
-                //                }
             }
     }
 }
