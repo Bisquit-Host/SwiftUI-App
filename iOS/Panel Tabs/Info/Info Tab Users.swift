8737c4f20b10a32b8036fe69f0cda783366aa622
TopScrech <sergei_saliukov@icloud.com>
2025-06-15 09:37:25 +0200

    improved info tab users button

--- iOS/Panel Tabs/Info/Info Tab Users.swift
+++ iOS/Panel Tabs/Info/Info Tab Users.swift
@@ -6,53 +6,50 @@ struct InfoTabUsers: View {
     @State private var sheetUsers = false
     
     var body: some View {
-        Menu {
+        Button {
+            sheetUsers = true
+        } label: {
+            VStack(spacing: 12) {
+                if vm.users.count == 0 {
+                    VStack(spacing: 5) {
+                        Image(systemName: "person.3.fill")
+                            .tertiary()
+                        
+                        Text("Users")
+                            .semibold()
+                    }
+                    .footnote()
+                } else {
+                    Text("Users")
+                        .footnote(.semibold)
+                    
+                    HStack {
+                        ForEach(vm.users.prefix(7)) { user in
+                            InfoTabButtonsUserImg(user.image)
+                        }
+                    }
+                }
+            }
+            .padding(.vertical)
+            .frame(maxWidth: .infinity)
+            .foregroundStyle(.foreground)
+            .background(.ultraThinMaterial, in: .rect(cornerRadius: 16))
+            .overlay {
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(.gray.opacity(0.25), lineWidth: 1)
+            }
+        }
+        .sheet($sheetUsers) {
+            UserListParent()
+                .environment(vm)
+        }
+        .contextMenu {
             Button {
                 sheetUsers = true
                 vm.sheetInvitation = true
             } label: {
                 Label("New user", systemImage: "person.badge.plus")
             }
-        } label: {
-            Button {
-                sheetUsers = true
-            } label: {
-                VStack(spacing: 12) {
-                    if vm.users.count == 0 {
-                        VStack(spacing: 5) {
-                            Image(systemName: "person.3.fill")
-                                .tertiary()
-                            
-                            Text("Users")
-                                .semibold()
-                        }
-                        .footnote()
-                    } else {
-                        Text("Users")
-                            .footnote(.semibold)
-                        
-                        HStack {
-                            ForEach(vm.users.prefix(7)) { user in
-                                InfoTabButtonsUserImg(user.image)
-                            }
-                        }
-                    }
-                }
-                .padding(.vertical)
-                .frame(maxWidth: .infinity)
-                .foregroundStyle(.foreground)
-                .background(.ultraThinMaterial, in: .rect(cornerRadius: 16))
-                .overlay {
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(.gray.opacity(0.25), lineWidth: 1)
-                }
-            }
-        } primaryAction: {
-            sheetUsers = true
-        }
-        .sheet($sheetUsers) {
-            UserListParent()
-                .environment(vm)
         }
     }
 }
