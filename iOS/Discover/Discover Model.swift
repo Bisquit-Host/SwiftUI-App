1d2505b9ceb185267a30e827676b11f947412c8f
TopScrech <sergei_saliukov@icloud.com>
2025-06-30 04:35:49 +0200

    -

--- iOS/Discover/Discover Model.swift
+++ iOS/Discover/Discover Model.swift
@@ -3,11 +3,14 @@ import SwiftUI
 struct DiscoverModel: Identifiable {
     let id = UUID()
     
-    let title: LocalizedStringKey
-    let subtitle: LocalizedStringKey
+    let title, subtitle: LocalizedStringKey
     let image: ImageResource
     
-    init(_ title: LocalizedStringKey, subtitle: LocalizedStringKey, image: ImageResource) {
+    init(
+        _ title: LocalizedStringKey,
+        subtitle: LocalizedStringKey,
+        image: ImageResource
+    ) {
         self.title = title
         self.subtitle = subtitle
         self.image = image
