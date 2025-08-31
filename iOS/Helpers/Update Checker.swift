fb4ab722762938aaaa836f06ef5ba125dc6d7ad0
TopScrech <sergei_saliukov@icloud.com>
2025-08-31 02:21:59 +0200

    -

--- iOS/Helpers/Update Checker.swift
+++ iOS/Helpers/Update Checker.swift
@@ -5,21 +5,24 @@ final class UpdateChecker {
     var alertUpdate = false
     
     func checkForUpdates() async {
-        let decoder = JSONDecoder()
-        var appStoreVersion = "0"
+        let path = "https://itunes.apple.com/lookup?bundleId=host.bisquit.Bisquit-Host"
         
         guard
             let currentVersion = Bundle.version,
-            let url = URL(string: "https://itunes.apple.com/lookup?bundleId=host.bisquit.Bisquit-Host")
+            let url = URL(string: path)
         else {
             return
         }
         
         let request = URLRequest(url: url)
+        var appStoreVersion = "0"
         
         do {
             let (data, _) = try await URLSession.shared.data(for: request)
+            
+            let decoder = JSONDecoder()
             let decoded = try decoder.decode(ItunesAppInfo.self, from: data)
+            
             appStoreVersion = decoded.results.first?.version ?? "0"
         } catch {
             return
