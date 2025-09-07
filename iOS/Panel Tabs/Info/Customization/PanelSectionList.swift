f5f4535ba30a551802e928feb856f28e8d866a2e
TopScrech <sergei_saliukov@icloud.com>
2025-09-08 00:59:29 +0200

    refactoring

---
+++ iOS/Panel Tabs/Info/Customization/PanelSectionList.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct PanelSectionList: View {
+    @Environment(PanelSectionVM.self) private var vm
+    
+    var body: some View {
+        List {
+            ForEach(vm.sections) { item in
+                PanelSectionRow(item) {
+                    vm.toggle(item)
+                    vm.save()
+                }
+                .listRowBackground(Color.clear)
+                .listRowSeparator(.hidden)
+            }
+            .onMove { here, there in
+                vm.move(from: here, to: there)
+            }
+        }
+        .navigationTitle("Customize & Reorder")
+        .navigationSubtitle("Reorder or hide sections to personalize your view")
+        .environment(\.editMode, .constant(.active))
+        .panelSectionListToolbar()
+    }
+}
+
+#Preview {
+    NavigationStack {
+        PanelSectionList()
+    }
+    .environment(PanelSectionVM())
+}
