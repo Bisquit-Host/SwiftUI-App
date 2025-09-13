ab323a65c2f7d1000ad912fd269011871ceadaab
TopScrech <sergei_saliukov@icloud.com>
2025-09-13 11:49:15 +0200

    #54 connected the ServerListVM

--- macOS/Redesign/Sidebar/DashboardSidebar.swift
+++ macOS/Redesign/Sidebar/DashboardSidebar.swift
@@ -1,15 +1,24 @@
 import SwiftUI
 
 struct DashboardSidebar: View {
+    @State private var vm = ServerListVM()
     @Binding var selection: SidebarItem?
+    @EnvironmentObject private var store: ValueStore
     
     var body: some View {
         List(selection: $selection) {
             Section {
-                ServerListGrid([PreviewProp.serverAttributes])
+                ServerListGrid(vm.filteredServers)
             }
         }
         .listStyle(.sidebar)
         .frame(minWidth: 400)
+        .onFirstAppear {
+            vm.loadServers()
+        }
+        .refreshableTask {
+            await vm.fetchServers(store.adminServerList)
+            store.updateServers.toggle()
+        }
     }
 }
