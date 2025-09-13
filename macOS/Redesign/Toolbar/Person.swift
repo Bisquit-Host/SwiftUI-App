f56c107b57611d56279953d5a8844013b4c0f806
TopScrech <sergei_saliukov@icloud.com>
2025-09-14 01:08:37 +0200

    -

--- macOS/Redesign/Toolbar/Person.swift
+++ macOS/Redesign/Toolbar/Person.swift
@@ -2,11 +2,18 @@ import SwiftUI
 
 struct Person: Identifiable {
     let id = UUID()
+    
     let name: String
     let initials: String
     let tint: Color
     
-    static let michael = Person(name: "Michael M", initials: "MM", tint: .orange)
-    static let john = Person(name: "John C", initials: "JC", tint: .purple)
-    static let dawne = Person(name: "Dawne A", initials: "DA", tint: .blue)
+    init(_ name: String, initials: String, tint: Color) {
+        self.name = name
+        self.initials = initials
+        self.tint = tint
+    }
+    
+    static let michael = Person("Michael M", initials: "MM", tint: .orange)
+    static let john = Person("John C", initials: "JC", tint: .purple)
+    static let dawne = Person("Dawne A", initials: "DA", tint: .blue)
 }
