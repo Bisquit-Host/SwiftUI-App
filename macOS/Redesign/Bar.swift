5bd5d8ac0356eda79cd3fe2f846b95c8206c48db
TopScrech <sergei_saliukov@icloud.com>
2025-09-15 12:07:09 +0200

    -

--- macOS/Redesign/Bar.swift
+++ macOS/Redesign/Bar.swift
@@ -2,14 +2,20 @@ import SwiftUI
 
 struct Bar: Identifiable {
     let id = UUID()
+    
     let label: String
     let value: Int
     
+    init(_ label: String, value: Int) {
+        self.label = label
+        self.value = value
+    }
+    
     static let sample = [
-        Bar(label: "Mon", value: 82),
-        Bar(label: "Tue", value: 51),
-        Bar(label: "Wed", value: 86),
-        Bar(label: "Thu", value: 45),
-        Bar(label: "Fri", value: 82)
+        Bar("Mon", value: 82),
+        Bar("Tue", value: 51),
+        Bar("Wed", value: 86),
+        Bar("Thu", value: 45),
+        Bar("Fri", value: 82)
     ]
 }
