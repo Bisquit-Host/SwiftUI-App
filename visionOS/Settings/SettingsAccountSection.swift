9ccddd9cafb2545cb3d76918f5ff086389962282
TopScrech <sergei_saliukov@icloud.com>
2025-09-29 10:25:34 +0200

    visionOS: SettingsAccountSection split into a separate view

---
+++ visionOS/Settings/SettingsAccountSection.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+import SwiftData
+import PteroNet
+
+struct SettingsAccountSection: View {
+    @Environment(NavState.self) private var navState
+    @EnvironmentObject private var store: ValueStore
+    @Query(animation: .default) private var keys: [APIKey]
+    
+    @State private var sheetKeyStorage = false
+    
+    var body: some View {
+        Section {
+            if keys.count > 0 {
+                Button("Switch account", systemImage: "arrow.trianglehead.2.clockwise.rotate.90") {
+                    sheetKeyStorage = true
+                }
+            }
+            
+            Button {
+                navState.clear()
+                store.isApiKeyValid = false
+                Keychain.delete(key: "selectedApiKey")
+            } label: {
+                Label {
+                    Text("Log out")
+                } icon: {
+                    Image(systemName: "rectangle.portrait.and.arrow.right")
+                        .foregroundStyle(.red)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    List {
+        SettingsAccountSection()
+    }
+    .environment(NavState())
+    .environmentObject(ValueStore())
+}
