4dc0362e6fb793c3c7f27afbb7bb14b6c766b79b
TopScrech <sergei_saliukov@icloud.com>
2025-11-04 23:54:14 +0100

    -

--- iOS/Server List/Server Card/ServerCardParent.swift
+++ iOS/Server List/Server Card/ServerCardParent.swift
@@ -11,13 +11,6 @@ struct ServerCardParent: View {
         self.server = server
     }
     
-//    @State private var showSafari = false
-//    @State private var confirmKill = false
-//    
-//    private var serverUrl: String {
-//        "https://mgr.bisquit.host/server/\(server.id)"
-//    }
-    
     var body: some View {
         VStack {
             Button {
@@ -33,24 +26,6 @@ struct ServerCardParent: View {
         }
         .buttonStyle(.plain)
         .hoverEffect()
-//        .safariCover($showSafari, url: serverUrl)
-//        .confirmationDialog("Perform kill action", isPresented: $confirmKill, titleVisibility: .visible) {
-//            Button("Kill", role: .destructive) {
-//                Task {
-//                    await PteroNet.powerSignal(server.id, do: .kill)
-//                }
-//            }
-//        }
-//        .contextMenu {
-//            ServerCardContextMenu(server, $showSafari, $confirmKill)
-//        }
-//        .onDrag {
-//            if let url = URL(string: serverUrl), let itemProvider = NSItemProvider(contentsOf: url) {
-//                itemProvider
-//            } else {
-//                NSItemProvider()
-//            }
-//        }
     }
 }
 
