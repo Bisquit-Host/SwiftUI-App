a6f56664703fe37f7d83416a6cf56a7c0f8e0bf0
TopScrech <sergei_saliukov@icloud.com>
2025-11-05 18:44:36 +0100

    visionOS: removed Archieving.swift & split QuickLookViewVM into a separate file

---
+++ visionOS/Panel/Files/QuickLookViewVM.swift
@@ -0,0 +1,49 @@
+import Foundation
+import PteroNet
+
+@Observable
+final class QuickLookViewVM {
+    var fileURL: URL? = nil
+    
+    func fetchDownloadUrl(_ id: String?, file: String, at root: String?) async {
+        guard let id, let root else {
+            return
+        }
+        
+        do {
+            let url = try await fileDownloadAPI(id, path: root + "/" + file)
+            downloadFile(url, name: file)
+        } catch {
+            SystemAlert.error(error)
+        }
+    }
+    
+    private func downloadFile(_ urlString: String, name: String) {
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
+        
+        let tempDirURL = FileManager.default.temporaryDirectory
+        let destinationURL = tempDirURL.appendingPathComponent(name)
+        
+        URLSession.shared.downloadTask(with: url) { location, _, error in
+            guard let location, error == nil else {
+                return
+            }
+            
+            do {
+                if FileManager.default.fileExists(atPath: destinationURL.path) {
+                    try FileManager.default.removeItem(at: destinationURL)
+                }
+                
+                try FileManager.default.copyItem(at: location, to: destinationURL)
+                
+                self.fileURL = destinationURL
+            } catch {
+                print("Error during file copy:", error.localizedDescription)
+            }
+        }
+        .resume()
+    }
+}
