55d9d1ac11065921e3a59e97f807d70cd9a77ed8
TopScrech <sergei_saliukov@icloud.com>
2025-11-27 05:12:32 +0100

    resource usage in the panel fixed

--- iOS/Panel/Websocket/ServerStats.swift
+++ iOS/Panel/Websocket/ServerStats.swift
@@ -1,17 +1,23 @@
 struct ServerStats: Codable {
-    let network: Network
+    let network: ServerStatsNetwork
     let state: String
-    let cpu, memory, disk: Double
-    let memoryLimitBytes, uptime: Int
-    
-    struct Network: Codable {
-        let rxBytes, txBytes: Int
-    }
+    let cpu: Double
+    let disk, memory, memoryLimit, uptime: Int
     
     enum CodingKeys: String, CodingKey {
-        case state, uptime, network, memoryLimitBytes,
+        case state, uptime, network,
              disk = "disk_bytes",
              cpu = "cpu_absolute",
-             memory = "memory_bytes"
+             memory = "memory_bytes",
+             memoryLimit = "memory_limit_bytes"
+    }
+}
+
+struct ServerStatsNetwork: Codable {
+    let rxBytes, txBytes: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case rxBytes = "rx_bytes",
+             txBytes = "tx_bytes"
     }
 }
