e12bd788eff3a688da45e760a75407249c692a18
TopScrech <sergei_saliukov@icloud.com>
2025-12-11 11:58:08 +0100

    added wiki web view

--- iOS/Billing/Support/SupportView.swift
+++ iOS/Billing/Support/SupportView.swift
@@ -1,20 +1,31 @@
 import SwiftUI
+import WebKit
 
 struct SupportView: View {
     @State private var vm = TicketListVM()
     
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
-            Tab("Tickets", systemImage: "lifepreserver") {
+        TabView(selection: $selectedTab) {
+            Tab("Tickets", systemImage: "questionmark.bubble", value: 0) {
                 TicketList()
+            }
+            
+            Tab("Wiki", systemImage: "books.vertical", value: 1) {
+                if let url = URL(string: "https://wiki.bisquit.host") {
+                    WebView(url: url)
+                }
             }
         }
         .navigationTitle("Support")
         .environment(vm)
         .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button("New", systemImage: "plus") {
-                    vm.createNewTicket()
+            if selectedTab == 0 {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("New", systemImage: "plus") {
+                        vm.createNewTicket()
+                    }
                 }
             }
         }
