f56e0f4ccec1db2ecd430238ddf3094d03085952
TopScrech <sergei_saliukov@icloud.com>
2025-12-18 12:07:15 +0100

    split AccountSettingsSwitchAccountButton into a separate view

--- iOS/Settings/Pterodactyl/Account/AccountSettings.swift
+++ iOS/Settings/Pterodactyl/Account/AccountSettings.swift
@@ -1,33 +1,15 @@
 import SwiftUI
-import SwiftData
 import PteroNet
 
 struct AccountSettings: View {
-    @Environment(ServerListVM.self) private var vm
     @Environment(NavState.self) private var nav
     @EnvironmentObject private var store: ValueStore
-    @Query(animation: .default) private var keys: [APIKey]
-    
-    @State private var sheetKeyStorage = false
     
     var body: some View {
-        @Bindable var vm = vm
-        
         BillingSectionCard("Account") {
-            if keys.count > 0 {
-                GlassyActionCard("Switch account", icon: "person.crop.circle", actionIcon: "chevron.up.chevron.down", tint: .purple) {
-                    sheetKeyStorage = true
-                }
-                .sheet($sheetKeyStorage) {
-                    CloudKeysParent($vm.apiKey) {
-                        await vm.fetchServers(store.adminServerList)
-                    }
-                }
-            }
-            
+            AccountSettingsSwitchAccountButton()
             GlassyActionCard("Log out", icon: "rectangle.portrait.and.arrow.right", tint: .red, role: .destructive, action: logout)
         }
-        
     }
     
     private func logout() {
---
+++ iOS/Settings/Pterodactyl/Account/AccountSettingsSwitchAccountButton.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import SwiftData
+
+struct AccountSettingsSwitchAccountButton: View {
+    @Environment(ServerListVM.self) private var vm
+    @Query(animation: .default) private var keys: [APIKey]
+    @EnvironmentObject private var store: ValueStore
+    
+    @State private var sheetKeyStorage = false
+    
+    var body: some View {
+        @Bindable var vm = vm
+        
+        if keys.count > 0 {
+            GlassyActionCard("Switch account", icon: "person.crop.circle", actionIcon: "chevron.up.chevron.down", tint: .purple) {
+                sheetKeyStorage = true
+            }
+            .sheet($sheetKeyStorage) {
+                CloudKeysParent($vm.apiKey) {
+                    await vm.fetchServers(store.adminServerList)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AccountSettingsSwitchAccountButton()
+        .darkSchemePreferred()
+}
