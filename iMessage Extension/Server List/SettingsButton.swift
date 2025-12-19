e3946eedbebaa48557531c6693c4727403557b35
TopScrech <sergei_saliukov@icloud.com>
2025-12-19 01:59:27 +0100

    removed outdated & unused ActionParent

--- iMessage Extension/Server List/SettingsButton.swift
+++ iMessage Extension/Server List/SettingsButton.swift
@@ -5,16 +5,12 @@ struct SettingsButton: View {
     @Environment(ServerListVM.self) private var vm
     //    @Environment(NavState.self) private var nav
     
-    @State private var sheetAccount = false
     @State private var sheetSettings = false
     
     var body: some View {
         @Bindable var vm = vm
         
         Menu {
-            Button("Account", systemImage: "person.crop.circle") {
-                sheetAccount = true
-            }
 #warning("iMessage: Settings and API-keys")
             //            Button("API-keys", systemImage: "key") {
             //                sheetKeyStorage = true
@@ -33,9 +29,6 @@ struct SettingsButton: View {
             //            }
         } label: {
             Image(systemName: "person.crop.circle")
-        }
-        .sheet($sheetAccount) {
-            AccountParent()
         }
         .sheet($sheetSettings) {
             NavigationStack {
