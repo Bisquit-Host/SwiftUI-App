08b5369ebcda0d3b0e270675318d8f0864e42cd7
TopScrech <sergei_saliukov@icloud.com>
2026-01-15 01:10:09 +0100

    refactor

--- iOS/Billing/My Services/MyServiceListVM.swift
+++ iOS/Billing/My Services/MyServiceListVM.swift
@@ -21,24 +21,24 @@ final class MyServiceListVM {
     }
     
     func fetchMyCloudServices() async {
-        await fetchServices(endpointPath: "cloud", isLoadingKeyPath: \.isCloudLoading) { data in
+        await fetchMyServices(endpointPath: "cloud", isLoadingKeyPath: \.isCloudLoading) { data in
             self.cloudServices = try BigAssDecoder.decode([CloudServiceSummary].self, from: data)
         }
     }
     
     func fetchMyGameServices() async {
-        await fetchServices(endpointPath: "game", isLoadingKeyPath: \.isGameLoading) { data in
+        await fetchMyServices(endpointPath: "game", isLoadingKeyPath: \.isGameLoading) { data in
             self.gameServices = try BigAssDecoder.decode([BillingGameServiceSummary].self, from: data)
         }
     }
     
     func fetchMyBotServices() async {
-        await fetchServices(endpointPath: "bot", isLoadingKeyPath: \.isBotLoading) { data in
+        await fetchMyServices(endpointPath: "bot", isLoadingKeyPath: \.isBotLoading) { data in
             self.botServices = try BigAssDecoder.decode([BillingBotServiceSummary].self, from: data)
         }
     }
     
-    private func fetchServices(
+    private func fetchMyServices(
         endpointPath: String,
         isLoadingKeyPath: ReferenceWritableKeyPath<MyServiceListVM, Bool>,
         assign: @escaping (Data) throws -> Void
