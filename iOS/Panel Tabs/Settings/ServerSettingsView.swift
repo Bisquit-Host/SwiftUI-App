4940ca121f63a208c159b8a765bc812b3b4d430b
TopScrech <sergei_saliukov@icloud.com>
2026-02-09 09:43:42 +0100

    split ServerSettingsReinstall

--- iOS/Panel Tabs/Settings/ServerSettingsView.swift
+++ iOS/Panel Tabs/Settings/ServerSettingsView.swift
@@ -12,8 +12,6 @@ struct ServerSettingsView: View {
         vm = ServerSettingsVM(server.id)
     }
     
-    @State private var alertReinstall = false
-    
     var body: some View {
         @Bindable var vm = vm
         
@@ -26,10 +24,8 @@ struct ServerSettingsView: View {
                 TextField("Server description", text: $vm.serverDescription)
                 
                 if vm.serverName != server.name || vm.serverDescription != server.description {
-                    Button("Save") {
-                        save()
-                    }
-                    .animation(.default, value: vm.serverName + vm.serverDescription)
+                    Button("Save", action: save)
+                        .animation(.default, value: vm.serverName + vm.serverDescription)
                 }
             }
             
@@ -38,19 +34,7 @@ struct ServerSettingsView: View {
                     .environment(vm)
             }
             
-            Section {
-                Button(role: .destructive) {
-                    alertReinstall = true
-                } label: {
-                    HStack {
-                        Text("Reinstall")
-                        
-                        Spacer()
-                        
-                        Image(systemName: "arrow.triangle.2.circlepath")
-                    }
-                }
-            }
+            ServerSettingsReinstall(server.id)
         }
         .navigationTitle("Server Settings")
 #if os(iOS) || os(macOS) || os(visionOS)
@@ -67,24 +51,11 @@ struct ServerSettingsView: View {
                 await panelVM.fetchServerDetails()
             }
         }
-        .alert("Reinstall Server", isPresented: $alertReinstall) {
-            Button("Reinstall", role: .destructive, action: reinstall)
-        } message: {
-            Text("Reinstalling your server will stop it, and then re-run the installation script that initially set it. Some files may be deleted or modified during this process, please back up your data before continuing")
-        }
     }
     
     private func save() {
         Task {
             await vm.serverRename()
-        }
-    }
-    
-    private func reinstall() {
-        Task {
-            await PteroNet.reinstallServer(server.id) {
-                SystemAlert.reinstalled()
-            }
         }
     }
 }
