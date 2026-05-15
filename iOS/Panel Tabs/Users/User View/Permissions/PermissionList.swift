aa35cec3114ce341857d99510ab5e4a88baefdf4
TopScrech <sergei_saliukov@icloud.com>
2026-05-15 08:37:40 +0200

    refactor

--- iOS/Panel Tabs/Users/User View/Permissions/PermissionList.swift
+++ iOS/Panel Tabs/Users/User View/Permissions/PermissionList.swift
@@ -12,134 +12,17 @@ struct PermissionList: View {
     
     @State private var showDescription = false
     
-    private var userPermissionsDict: [String: Bool] {
-        var dict: [String: Bool] = [:]
-        
-        if let permissions = vm.permissions {
-            permissions.permissions.forEach { key, permission in
-                permission.keys.keys.forEach { subKey in
-                    let fullKey = "\(key).\(subKey)"
-                    
-                    dict[fullKey] = user.permissions.contains(fullKey)
-                }
-            }
-        }
-        
-        return dict
-    }
-    
     var body: some View {
         if let permissions = vm.permissions {
             PermissionGroupHeader($showDescription, user: user)
             
-            PermissionsList(
-                permissions: permissions.permissions,
-                showDescription: $showDescription,
-                user: $user,
-                userPermissionsDict: userPermissionsDict
-            )
-        }
-    }
-}
-
-struct PermissionsList: View {
-    var permissions: [String: Permission]
-    @Binding var showDescription: Bool
-    @Binding var user: UserAttributes
-    var userPermissionsDict: [String: Bool]
-    
-    var body: some View {
-        ForEach(permissions.keys.sorted(), id: \.self) { key in
-            PermissionSection(
-                key: key,
-                permission: permissions[key],
-                showDescription: $showDescription,
-                user: $user,
-                userPermissionsDict: userPermissionsDict
-            )
-        }
-    }
-}
-
-struct PermissionSection: View {
-    var key: String
-    var permission: Permission?
-    @Binding var showDescription: Bool
-    @Binding var user: UserAttributes
-    var userPermissionsDict: [String: Bool]
-    
-    @State private var showTranslation = false
-    
-    var body: some View {
-        if let permission {
-            Section {
-                ForEach(permission.keys.keys.sorted(), id: \.self) { subKey in
-                    PermissionCard(
-                        key: key,
-                        subKey: subKey,
-                        subValue: permission.keys[subKey],
-                        showDescription: $showDescription,
-                        user: $user,
-                        userPermissionsDict: userPermissionsDict
-                    )
-                }
-            } header: {
-                Text(key.capitalized)
-            } footer: {
-                if showDescription {
-                    Text(permission.description)
-                        .secondary()
-                        .multilineTextAlignment(.leading)
-#if os(iOS) || os(macOS)
-                        .translationPresentation($showTranslation, text: permission.description)
-                        .onTapGesture {
-                            showTranslation = true
-                        }
-#endif
-                }
-            }
-#if os(tvOS)
-            Divider()
-#endif
-        }
-    }
-}
-
-struct PermissionCard: View {
-    var key: String
-    var subKey: String
-    var subValue: String?
-    @Binding var showDescription: Bool
-    @Binding var user: UserAttributes
-    var userPermissionsDict: [String: Bool]
-    
-    @State private var showTranslation = false
-    
-    var body: some View {
-        if let subValue {
-            let perm = userPermissionsDict["\(key).\(subKey)"]
-            
-            VStack(alignment: .leading) {
-                PermissionToggle(
-                    userPermissions: user.permissions,
-                    user: $user,
+            ForEach(permissions.permissions.keys.sorted(), id: \.self) { key in
+                PermissionSection(
                     key: key,
-                    subKey: subKey,
-                    perm: perm
+                    permission: permissions.permissions[key],
+                    showDescription: $showDescription,
+                    user: $user
                 )
-                
-                if showDescription {
-                    Text(subValue)
-                        .caption2()
-                        .secondary()
-                        .multilineTextAlignment(.leading)
-#if os(iOS) || os(macOS)
-                        .translationPresentation($showTranslation, text: subValue)
-                        .onTapGesture {
-                            showTranslation = true
-                        }
-#endif
-                }
             }
         }
     }
