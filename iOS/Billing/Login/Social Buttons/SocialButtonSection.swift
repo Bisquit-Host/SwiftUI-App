e350dbcda3524592049227cd2aca780b813a22db
TopScrech <sergei_saliukov@icloud.com>
2026-05-21 01:12:07 +0200

    refactor

--- iOS/Billing/Login/Social Buttons/SocialButtonSection.swift
+++ iOS/Billing/Login/Social Buttons/SocialButtonSection.swift
@@ -5,34 +5,18 @@ struct SocialButtonSection: View {
     
     var body: some View {
         HStack(alignment: .top) {
-            providerButton("GitHub", img: .gitHub, isLastUsed: vm.lastUsedProvider == .github) {
+            SocialButton(provider: "GitHub", img: .gitHub, isLastUsed: vm.lastUsedProvider == .github) {
                 vm.startGitHubLinking()
             }
             
-            providerButton("Google", img: .google, isLastUsed: vm.lastUsedProvider == .google) {
+            SocialButton(provider: "Google", img: .google, isLastUsed: vm.lastUsedProvider == .google) {
                 vm.startGoogleLinking()
             }
             
-            providerButton("Yandex", img: .yandex, isLastUsed: vm.lastUsedProvider == .yandex) {
+            SocialButton(provider: "Yandex", img: .yandex, isLastUsed: vm.lastUsedProvider == .yandex) {
                 vm.startYandexLinking()
             }
         }
-    }
-    
-    private func providerButton(_ name: String, img: ImageResource, isLastUsed: Bool, action: @escaping () -> Void) -> some View {
-        SocialButton(provider: name, img: img, action: action)
-            .overlay(alignment: .bottom) {
-                if isLastUsed {
-                    Text("Last used")
-                        .caption2(.semibold)
-                        .padding(.horizontal, 10)
-                        .padding(.vertical, 4)
-#if !os(visionOS)
-                        .glassEffect()
-#endif
-                        .offset(y: 14)
-                }
-            }
     }
 }
 
