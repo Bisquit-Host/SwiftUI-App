dc732a81092ebf77e4f8406048877275569e6ef2
TopScrech <sergei_saliukov@icloud.com>
2026-05-29 11:27:21 +0200

    improved SocialButtonBadge

--- iOS/Billing/Login/Social Buttons/SocialButtonBadge.swift
+++ iOS/Billing/Login/Social Buttons/SocialButtonBadge.swift
@@ -1,16 +1,16 @@
-import SwiftUI
+import ScrechKit
 
 struct SocialButtonBadge: View {
     var body: some View {
         Image(systemName: "star.fill")
             .font(.caption.bold())
             .foregroundStyle(.yellow)
-            .frame(width: 24, height: 24)
+            .frame(24)
 #if !os(visionOS)
             .glassEffect()
 #endif
             .clipShape(.circle)
-            .offset(x: 7, y: -7)
+            .offset(x: 6, y: -5)
             .accessibilityLabel("Last used")
     }
 }
