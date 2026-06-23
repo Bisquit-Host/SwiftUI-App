39b4846c81bc3b568d92dd22f1a8308a27ab3e10
TopScrech <sergei_saliukov@icloud.com>
2026-06-23 13:14:18 +0200

    Fixed sidebar automatically opening behaviour on iPhone 12/13 mini

--- iOS/Panel/Sidebar/PanelAdaptiveView.swift
+++ iOS/Panel/Sidebar/PanelAdaptiveView.swift
@@ -6,11 +6,17 @@ struct PanelAdaptiveView<Content: View>: View {
     @ViewBuilder var content: (CGSize, Bool) -> Content
     
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    @Environment(\.verticalSizeClass) private var verticalSizeClass
     
     var body: some View {
         GeometryReader {
             let size = $0.size
-            let isLandscape = size.width > size.height || (horizontalSizeClass == .regular && showsSideBarOniPadPortrait)
+            let isLandscape =
+                if horizontalSizeClass == .compact {
+                    verticalSizeClass == .compact
+                } else {
+                    horizontalSizeClass == .regular && (showsSideBarOniPadPortrait || verticalSizeClass == .compact)
+                }
             
             content(size, isLandscape)
         }
