0a4262591689ad98cd9ec2d2de94b5df33d17a3a
TopScrech <sergei_saliukov@icloud.com>
2026-06-26 16:28:23 +0200

    improved emoty views in installed mods & plugins

--- iOS/Panel Tabs/Mod Installer/Installed Mods/InstalledModList.swift
+++ iOS/Panel Tabs/Mod Installer/Installed Mods/InstalledModList.swift
@@ -10,25 +10,31 @@ struct InstalledModList: View {
     
     var body: some View {
         ScrollView {
-            BillingSectionCard(showsBackground: false) {
-                if vm.installedMods.isEmpty {
-                    Text("No installed mods")
-                        .secondary()
-                } else {
+            if !vm.installedMods.isEmpty {
+                BillingSectionCard(showsBackground: false) {
                     VStack(alignment: .leading, spacing: 8) {
                         ForEach(vm.installedMods) {
                             InstalledModCard($0, canUpdate: canUpdate, installModUpdate: installModUpdate)
                         }
                     }
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .backgroundStyling(store.panelSidebarBackgroundStyle, in: .rect(cornerRadius: 16))
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .backgroundStyling(store.panelSidebarBackgroundStyle, in: .rect(cornerRadius: 16))
         }
         .padding()
         .scrollIndicators(.never)
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(BackgroundImage())
+        .overlay {
+            if vm.installedMods.isEmpty {
+                ContentUnavailableView(
+                    "No installed mods",
+                    systemImage: "shippingbox.fill",
+                    description: Text("Installed mods will appear here")
+                )
+            }
+        }
     }
 }
 
--- iOS/Panel Tabs/Plugin Installer/Installed plugins/InstalledPluginList.swift
+++ iOS/Panel Tabs/Plugin Installer/Installed plugins/InstalledPluginList.swift
@@ -10,25 +10,31 @@ struct InstalledPluginList: View {
     
     var body: some View {
         ScrollView {
-            BillingSectionCard(showsBackground: false) {
-                if vm.installedPlugins.isEmpty {
-                    Text("No installed plugins")
-                        .secondary()
-                } else {
+            if !vm.installedPlugins.isEmpty {
+                BillingSectionCard(showsBackground: false) {
                     VStack(alignment: .leading) {
                         ForEach(vm.installedPlugins) {
                             InstalledPluginCard(plugin: $0, canUpdate: canUpdate, installPluginUpdate: installPluginUpdate)
                         }
                     }
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .backgroundStyling(store.panelSidebarBackgroundStyle, in: .rect(cornerRadius: 16))
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .backgroundStyling(store.panelSidebarBackgroundStyle, in: .rect(cornerRadius: 16))
         }
         .padding()
         .scrollIndicators(.never)
-        .frame(maxWidth: .infinity)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(BackgroundImage())
+        .overlay {
+            if vm.installedPlugins.isEmpty {
+                ContentUnavailableView(
+                    "No installed plugins",
+                    systemImage: "puzzlepiece.fill",
+                    description: Text("Installed plugins will appear here")
+                )
+            }
+        }
     }
 }
 
