dd5172e2742b2cf98624183ff6b0594a237f4821
TopScrech <sergei_saliukov@icloud.com>
2026-06-29 19:07:40 +0200

    added toolbar spacing in vds panel

--- iOS/Billing/My Services/Cloud/Details/VDSServiceDetailsToolbarModifier.swift
+++ iOS/Billing/My Services/Cloud/Details/VDSServiceDetailsToolbarModifier.swift
@@ -15,65 +15,73 @@ struct VDSServiceDetailsToolbarModifier: ViewModifier {
     func body(content: Content) -> some View {
         content.toolbar {
             if selectedTab == 0 {
-                Menu {
-                    Button("Start", systemImage: "play") {
-                        Task {
-                            await vm.power("start", serviceId: serviceId)
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Button("Start", systemImage: "play") {
+                            Task {
+                                await vm.power("start", serviceId: serviceId)
+                            }
                         }
-                    }
-                    
-                    Button("Stop", systemImage: "stop") {
-                        Task {
-                            await vm.power("stop", serviceId: serviceId)
+                        
+                        Button("Stop", systemImage: "stop") {
+                            Task {
+                                await vm.power("stop", serviceId: serviceId)
+                            }
                         }
-                    }
-                    
-                    Button("Restart", systemImage: "arrow.trianglehead.2.clockwise.rotate.90") {
-                        Task {
-                            await vm.power("restart", serviceId: serviceId)
+                        
+                        Button("Restart", systemImage: "arrow.trianglehead.2.clockwise.rotate.90") {
+                            Task {
+                                await vm.power("restart", serviceId: serviceId)
+                            }
                         }
+                    } label: {
+                        Image(systemName: "power")
+                            .foregroundStyle(vm.service?.state.color ?? .gray)
                     }
-                } label: {
-                    Image(systemName: "power")
-                        .foregroundStyle(vm.service?.state.color ?? .gray)
                 }
                 
-                Menu {
-                    Button("Rename", systemImage: "pencil") {
-                        pendingName = vm.service?.name ?? ""
-                        alertRename = true
-                    }
-                    
-                    Divider()
-                    
-                    if let password = vm.service?.password {
-                        Button("Copy password", systemImage: "document.on.document") {
-                            Pasteboard.copy(password)
-                            SystemAlert.copied()
+                ToolbarSpacer(.fixed, placement: .topBarTrailing)
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Button("Rename", systemImage: "pencil") {
+                            pendingName = vm.service?.name ?? ""
+                            alertRename = true
                         }
+                        
+                        Divider()
+                        
+                        if let password = vm.service?.password {
+                            Button("Copy password", systemImage: "document.on.document") {
+                                Pasteboard.copy(password)
+                                SystemAlert.copied()
+                            }
+                        }
+                        
+                        Button("Change password", systemImage: "lock") {
+                            alertChangePassword = true
+                        }
+                        
+                        Button("Reinstall OS", systemImage: "arrow.triangle.2.circlepath") {
+                            sheetReinstallOS = true
+                        }
+                    } label: {
+                        Image(systemName: "ellipsis")
                     }
-                    
-                    Button("Change password", systemImage: "lock") {
-                        alertChangePassword = true
-                    }
-                    
-                    Button("Reinstall OS", systemImage: "arrow.triangle.2.circlepath") {
-                        sheetReinstallOS = true
-                    }
-                } label: {
-                    Image(systemName: "ellipsis")
                 }
             } else if selectedTab == 3 {
-                Menu {
-                    Button("Change credentials", systemImage: "key") {
-                        sheetSSHCredentials = true
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        Button("Change credentials", systemImage: "key") {
+                            sheetSSHCredentials = true
+                        }
+                        
+                        Button("Logs", systemImage: "list.bullet.rectangle") {
+                            sheetSSHLogs = true
+                        }
+                    } label: {
+                        Image(systemName: "ellipsis")
                     }
-                    
-                    Button("Logs", systemImage: "list.bullet.rectangle") {
-                        sheetSSHLogs = true
-                    }
-                } label: {
-                    Image(systemName: "ellipsis")
                 }
             }
         }
