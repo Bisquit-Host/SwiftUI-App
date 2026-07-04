d505a80c79967d8cf0b31597ac6b068cf3b07814
TopScrech <89252798+TopScrech@users.noreply.github.com>
2026-07-05 00:33:17 +0200

    -

--- iOS/Panel Tabs/Console/ConsoleVM.swift
+++ iOS/Panel Tabs/Console/ConsoleVM.swift
@@ -4,11 +4,11 @@ import Calagopus
 @Observable
 final class ConsoleVM {
     private let id: String
-
+    
     init(_ id: String) {
         self.id = id
     }
-
+    
     var alertKill = false
     var inspectorPresented = false
     var commandHistoryPresented = false
@@ -17,7 +17,7 @@ final class ConsoleVM {
     var command = ""
     var fontSize = 10.0
     var lastMessageIndex = 0
-
+    
     func returnFontDesignName(_ fontDesign: Font.Design) -> String {
         switch fontDesign {
         case .default:    "Default"
@@ -27,23 +27,23 @@ final class ConsoleVM {
         default:          ""
         }
     }
-
+    
     func sendCommand() async {
         await CalagopusNet.sendCommand(id, command: command)
         command = ""
     }
-
+    
     func useHistoryCommand(_ command: String) {
         self.command = command
         commandHistoryPresented = false
     }
-
+    
     func fetchCommandHistory() async {
         commandHistoryLoading = true
         defer {
             commandHistoryLoading = false
         }
-
+        
         do {
             let client = try CalagopusClientFactory.client()
             let endpoint = try CalagopusGeneratedOperations.getApiClientServersServerActivity.endpoint(
