dd58230e167fea0978e811599a7b09ce451aa745
TopScrech <89252798+TopScrech@users.noreply.github.com>
2026-09-18 23:27:45 +0200

    removed dead comment

--- iOS/Panel/Sidebar/PanelSidebarList.swift
+++ iOS/Panel/Sidebar/PanelSidebarList.swift
@@ -41,7 +41,6 @@ struct PanelSidebarList: View {
             .padding(12)
         }
         .scrollIndicators(.never)
-//        .scrollEdgeEffectHidden(for: .top)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         .task {
             guard serverListVM.servers.isEmpty else {
